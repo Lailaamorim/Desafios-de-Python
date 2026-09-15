75f32151adfb74039b35b9e5973960b0dbf056d4
Laila Amorim <lailaamorimsant@gmail.com>
2026-09-15 20:11:25 -0300

    desafio 53 feito

--- Python 3 Mundo 01/Python 3 Mundo 02/Desafios/Desafio52_.py
+++ Python 3 Mundo 01/Python 3 Mundo 02/Desafios/Desafio52_.py
@@ -148,3 +148,16 @@
 #
 # Caso contrário,
 # ele não é primo.
+
+
+num = int(input("Digite um número: "))
+divisores = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        divisores += 1
+        
+if divisores  == 2:
+  print(f"O número {num} é PRIMO.")
+else:
+  print(f"O numero {num} NÃO É PRIMO")   
